9f2e00ea911872dda64d5b5ff1fcbe02eea86458
sahilsharma20 <137672866+sahilsharma20@users.noreply.github.com>
2024-07-20 14:47:45 +0530

    Add files via upload

---
+++ Day2_16july2024.py
@@ -0,0 +1,30 @@
+# var = 20  #integer
+# print(var)
+# print (type (var)) ##type tell the data type which is used before variable.
+
+
+# AGE = 10
+# Age = 20
+# age = 30
+
+# print ( AGE  )
+# print ( Age )
+# print ( age )
+
+
+# Var = 10
+# #1var = 10
+# var1 = 10
+# #var@ = 10
+# _ = 10
+# print (_)
+
+
+# st = "Upflairs"
+# print ( st [-4])
+# print ( st [::-1])
+
+
+st = "upflairs pvt ltd jaipur rajasthan"
+print ( st [-16:-10: ])
+print(len(st))
